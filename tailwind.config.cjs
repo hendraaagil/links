d49c05791f7926be6283964beff07c8eb7ec834e
hendraaagil <hendraaagil@gmail.com>
2023-01-16 20:28:23 +0700

    fix: remove unused config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,6 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
-      mono: ['Fira Code', 'monospace'],
       sans: ['Plus Jakarta Sans', 'sans-serif'],
     },
     extend: {
